c97874625a34a3e04ec86e8f0e5fcd900435d2e4
obogrelova <on.obogrelova@gmail.com>
2025-03-11 19:08:39 +0500

    New files

---
+++ flower_delivery/delivery/telegram_bot/bot.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+from telegram import Update
+from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes
+from aiogram import Bot
+from config import TOKEN, ADMIN_CHAT_ID
+
+bot = Bot(token=TOKEN)
+
+logging.basicConfig(level=logging.INFO)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    await context.bot.send_message(chat_id=chat_id, text=f'Your chat ID: {chat_id}')
+
+async def send_message_to_admin(text):
+    try:
+        await bot.send_message(ADMIN_CHAT_ID, text)
+    except Exception as e:
+        logging.error(f'Ошибка при отправке сообщения: {e}')
+
+def send_message(text):
+    asyncio.run(send_message_to_admin(text))
+
+async def main():
+    application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler('start', start))
+    await application.run_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- flower_delivery/flower_delivery/urls.py
+++ flower_delivery/flower_delivery/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from delivery.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authentication/', include('authentication.urls')),
+    path('delivery/', include('delivery.urls')),
+    path('', home, name='home'),
 ]
