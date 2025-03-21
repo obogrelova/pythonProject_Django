8258d1e466d1386a2b554a0fa350664e467e8361
obogrelova <on.obogrelova@gmail.com>
2025-03-21 23:03:27 +0500

    Update file

--- flower_delivery/authentication/forms.py
+++ flower_delivery/authentication/forms.py
@@ -5,7 +5,7 @@ from .models import User
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['name', 'email', 'password1', 'password2']
+        fields = ['name', 'email']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'id': 'name'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'id': 'email'}),
--- flower_delivery/authentication/models.py
+++ flower_delivery/authentication/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(verbose_name='Имя клиента', max_length=255)
-    email = models.EmailField(verbose_name='Электронная почта', unique=True)
+    email = models.EmailField(verbose_name='Электронная почта')
 
     def __str__(self):
         return self.name
