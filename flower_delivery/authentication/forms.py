76e693963145409f4578db2c65eb2893d0aa99c7
obogrelova <on.obogrelova@gmail.com>
2025-03-22 11:27:38 +0500

    Update files

--- flower_delivery/authentication/forms.py
+++ flower_delivery/authentication/forms.py
@@ -5,7 +5,7 @@ from .models import User
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['name', 'email']
+        fields = ['name', 'email', 'password1', 'password2']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'id': 'name'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'id': 'email'}),
