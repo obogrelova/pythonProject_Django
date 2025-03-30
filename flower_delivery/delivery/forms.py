1b1e321b8fd7e38b961292ea427999b1c4b2dfd7
obogrelova <on.obogrelova@gmail.com>
2025-03-30 09:06:28 +0500

    Update files

--- flower_delivery/delivery/forms.py
+++ flower_delivery/delivery/forms.py
@@ -2,20 +2,19 @@ from django import forms
 from .models import Product, Order
 
 class ProductForm(forms.ModelForm):
-    flowers = forms.CharField(label='Название', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    name = forms.CharField(label='Название', widget=forms.TextInput(attrs={'class': 'form-input'}))
     price = forms.DecimalField(label='Цена', widget=forms.TextInput(attrs={'class': 'form-input'}))
     image = forms.ImageField(label='Изображение', widget=forms.ClearableFileInput(attrs={'class': 'form-control-lg'}))
 
     class Meta:
         model = Product
-        fields = ['flowers', 'price', 'image']
+        fields = ['name', 'price', 'image']
 
 
 class OrderForm(forms.ModelForm):
     username = forms.CharField(label='Имя клиента', widget=forms.TextInput(attrs={'class': 'form-input'}))
     phone = forms.CharField(label='Телефон', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    flowers = forms.CharField(label='Название', widget=forms.TextInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = Order
-        fields = ['username', 'phone', 'flowers']
+        fields = ['username', 'phone']
--- flower_delivery/delivery/models.py
+++ flower_delivery/delivery/models.py
@@ -5,22 +5,19 @@ from django.conf import settings
 # Create your models here.
 
 class Product(models.Model):
-    flowers = models.CharField(max_length=150, verbose_name='Название')
+    name = models.CharField(max_length=150, verbose_name='Название')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name='Изображение')
 
     def __str__(self):
-        return self.flowers
+        return self.name
 
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     username = models.CharField(max_length=150, unique=True, verbose_name='Имя клиента')
     phone = models.CharField(max_length=30, verbose_name='Телефон')
-    flowers = models.CharField(max_length=150, verbose_name='Название')
     total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Общая цена')
-
-
 
     def __str__(self):
         return self.username
